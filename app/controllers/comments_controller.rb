503124067a29aad37a1d722f82145dfbbc2309e2
Lucas <lucas.odonnelll@gmail.com>
2021-04-07 17:26:25 -0700

    comments are on index page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to root_path
+  end
+
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :body, :comment_image, :post_id, :user_id)
+  end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many_attached :images, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
   validates :body, presence: true
 end
